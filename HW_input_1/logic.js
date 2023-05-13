1c05cf197481a9d46e52b0655afde2c0fae9b7b5
prestij1987 <94439314+prestij1987@users.noreply.github.com>
2023-05-13 09:47:54 +0300

    Add files via upload
    push folder

---
+++ HW_input_1/logic.js
@@ -0,0 +1,21 @@
+
+function my_age() {
+    document.getElementById('age').value
+};
+
+var button_color = 'Привет';
+var button_2 = 'Пока';
+function hello() {
+
+    document.getElementById('button_color').value= "Пока";
+    if (button_1 == 'Привет') {
+        return true;
+    }
+    else (button_2 =='Пока') 
+        return false;
+    
+
+};
+
+
+console.log(hello);
