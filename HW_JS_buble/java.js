739c6ee766d58841c3e06c57e8fb3dc87a1cb6b7
prestij1987 <94439314+prestij1987@users.noreply.github.com>
2023-05-17 07:00:56 +0300

    Add files via upload

---
+++ HW_JS_buble/java.js
@@ -0,0 +1,23 @@
+
+document.getElementById('bub_1').onclick='func_1';
+document.getElementById('bub_2').onclick='func_2';
+document.getElementById('bub_3').onclick='func_3';
+document.getElementById('bub_4').onclick='func_4';
+document.getElementById('bub_5').onclick='func_5';
+
+
+func_1(id) {
+    let first= document.getElementById('bub_1').style.display='none';
+    console.log(first);
+};
+
+func_2(id) {
+    let second= document.getElementById('bub_1').style.display='none';
+    console.log(second);
+};
+
+func_3(id) {
+    let first= document.getElementById('bub_1').style.display='none';
+    console.log(first);
+};
+
