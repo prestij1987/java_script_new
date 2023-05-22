8e8973989d716d26e838df12d3000bb315eb1f63
prestij1987 <94439314+prestij1987@users.noreply.github.com>
2023-05-22 14:10:13 +0300

    Add files via upload

---
+++ HW_JS_city/table_city.js
@@ -0,0 +1,61 @@
+let yo = 'hello, world';
+console.log(yo);
+
+
+
+
+let name_city = [];
+let name_country = [];
+
+let city = {'Россия' : 'Москва',
+            'Алжир' : 'Алжир',
+            'Италия' : 'Рим',
+            'Испания' : 'Мадрид',
+            'Ирландия' : 'Дублин',
+};
+// Создаем массив городов.
+
+// Создаем функцию создание элементовs
+
+function list (scroll) {
+    let city_1 = document.createElement('name');
+    city_1.setAttribute('value' + scroll);
+    city.id = name;
+    city.type = text;
+    
+
+};
+// Создание функции для нажатия кнопки
+function func_but(){
+	
+    let but_01 = document.createElement('city_1');
+	let = but_1 = document.querySelector('city_1').onclick = func_but();
+    but_1.setAttribute('onclick', 'save(this)');
+};
+function func_but2(){
+	
+    let but_02 = document.createElement('city_2');
+	let but_2 = document.querySelector('city_2').onclick = func_but1();
+    but_2.setAttribute('onclick', 'save(this)');
+};
+
+function func_but3(){
+    let but_03 = document.createElement('city_3');
+	let but_3 = document.querySelector('city_3').onclick = func_but2();
+    but_3.setAttribute('onclick', 'save(this)');
+};
+
+function func_but4(){
+    let but_04 = document.createElement('city_4');
+	let but_4 = document.querySelector('city_4').onclick = func_but3();
+    but_4.setAttribute('onclick', 'save(this)');
+};
+
+function func_but5(){
+    let but_05 = document.createElement('city_4');
+	let but_5 = document.querySelector('city_4').onclick = func_but4();
+    but_5.setAttribute('onclick', 'save(this)');	
+	
+};
+
+};
